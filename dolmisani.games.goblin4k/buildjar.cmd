e5ff1f9c44bfa8cb4502ad1ee2c7f691c7c96bef
daniele.olmisani <daniele.olmisani@9c54bd88-8c03-11dd-887c-ed04d4280016>
2009-04-05 11:58:56 +0000

    public release v0.2

--- dolmisani.games.goblin4k/buildjar.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-del goblin4k*.jar
-copy .\bin\G*.class
-.\resources\kzip\kzip.exe /s0 /r goblin4k-un.jar G*.class META-INF/MANIFEST.MF
-del G*.class
-java -jar resources/proguard4.3/lib/proguard.jar @goblin4k.pro
-dir *.jar
--- dolmisani.toys/src/dolmisani/toys/utils/ComplexFunction.java
+++
@@ -1,7 +0,0 @@
-package dolmisani.toys.utils;
-
-public interface ComplexFunction {
-
-		public Complex f(Complex[] z);
-	
-}
