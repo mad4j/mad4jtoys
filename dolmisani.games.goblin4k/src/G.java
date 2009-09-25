f89d718fbe05588c7a9bc5ba77c6ab10a142584c
daniele.olmisani <daniele.olmisani@9c54bd88-8c03-11dd-887c-ed04d4280016>
2009-09-25 21:50:41 +0000

    add support for sprite definition as VIC20 style

--- dolmisani.games.goblin4k/src/G.java
+++ dolmisani.games.goblin4k/src/G.java
@@ -18,7 +18,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 /*
  * Original Code
  * --------------------------------------------------------------------------
@@ -77,26 +76,23 @@ import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.sound.midi.MidiChannel;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Patch;
-import javax.sound.midi.Synthesizer;
 import javax.swing.JFrame;
 
 import sun.audio.AudioPlayer;
 
 /**
- *  Goblin4k is a remake of glorious game of the Commodore Vic20.
- *  This version is developped using the Java4k coding rules.
- *  
+ * Goblin4k is a remake of glorious game of the Commodore Vic20. This version is
+ * developped using the Java4k coding rules.
+ * 
  * @author Daniele Olmisani (daniele.olmisani@gmail.com)
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class G extends JFrame {
@@ -120,21 +116,19 @@ public class G extends JFrame {
 	private static final int TILE_ROCK = 0;
 	private static final int TILE_FACE_SAD = 1;
 	private static final int TILE_FACE_HAPPY = 2;
+	private static final int TILE_GOBLIN_PLAY = 3;
+	private static final int TILE_GOBLIN_CRASH = 4;
 
-	private static final int MAX_TILES = 3;
+	private static final int MAX_TILES = 5;
 
 	private static final int MAX_ROCKS = 65;
 	private static final int MAX_FACES = 20;
-	
 
 	private boolean key[] = new boolean[65535];
 	private boolean keyLock[] = new boolean[65535];
 
-	
-	private InputStream tone1;
-	
-	
-	
+	// private InputStream tone1;
+
 	public static void main(String[] args) {
 		new G();
 	}
@@ -159,9 +153,8 @@ public class G extends JFrame {
 
 		int playerX = 0;
 		int playerY = 0;
-		
-		int[][] board = new int[BOARD_WIDTH][BOARD_HEIGHT];
 
+		int[][] board = new int[BOARD_WIDTH][BOARD_HEIGHT];
 
 		// set the window size
 		getContentPane().setPreferredSize(
@@ -180,41 +173,32 @@ public class G extends JFrame {
 
 		Graphics2D g;
 
-		BufferedImage playerImage = new BufferedImage(TILE_SIZE, TILE_SIZE,
-				BufferedImage.TYPE_INT_ARGB);
-		drawGoblin(playerImage.createGraphics());
-
 		BufferedImage[] tiles = new BufferedImage[MAX_TILES];
+
+		tiles[TILE_GOBLIN_PLAY] = new BufferedImage(TILE_SIZE, TILE_SIZE,
+				BufferedImage.TYPE_INT_ARGB);
+		drawSprite(tiles[TILE_GOBLIN_PLAY].createGraphics(), Color.green
+				.darker(), new int[] { 126, 219, 219, 255, 165, 90, 90, 165 });
+
+		tiles[TILE_GOBLIN_CRASH] = new BufferedImage(TILE_SIZE, TILE_SIZE,
+				BufferedImage.TYPE_INT_ARGB);
+		drawSprite(tiles[TILE_GOBLIN_CRASH].createGraphics(), Color.red
+				.darker(), new int[] { 170, 85, 170, 85, 126, 219, 255, 189 });
 
 		tiles[TILE_ROCK] = new BufferedImage(TILE_SIZE, TILE_SIZE,
 				BufferedImage.TYPE_INT_ARGB);
-		drawRock(tiles[TILE_ROCK].createGraphics());
-		
+		drawSprite(tiles[TILE_ROCK].createGraphics(), Color.orange, new int[] {
+				170, 85, 170, 85, 170, 85, 170, 85 });
+
 		tiles[TILE_FACE_SAD] = new BufferedImage(TILE_SIZE, TILE_SIZE,
 				BufferedImage.TYPE_INT_ARGB);
-		drawFace(tiles[TILE_FACE_SAD].createGraphics(), false);
+		drawSprite(tiles[TILE_FACE_SAD].createGraphics(), Color.black,
+				new int[] { 60, 66, 165, 129, 153, 165, 66, 60 });
 
 		tiles[TILE_FACE_HAPPY] = new BufferedImage(TILE_SIZE, TILE_SIZE,
 				BufferedImage.TYPE_INT_ARGB);
-		drawFace(tiles[TILE_FACE_HAPPY].createGraphics(), true);
-
-		MidiChannel channel = null;
-		try {
-			Synthesizer synthesizer = MidiSystem.getSynthesizer();
-
-			synthesizer.open();
-
-			channel = synthesizer.getChannels()[0];
-			Patch patch = synthesizer.getAvailableInstruments()[407].getPatch();
-			channel.programChange(patch.getBank(), patch.getProgram());
-		} catch (Exception e) {
-		}
-
-		
-		tone1 = createSample();
-		
-		
-	
+		drawSprite(tiles[TILE_FACE_HAPPY].createGraphics(), Color.black,
+				new int[] { 60, 66, 165, 129, 165, 153, 66, 60 });
 
 		// TODO : Scenery
 		BufferedImage scenery = new BufferedImage(GAMEFILED_WIDTH,
@@ -231,31 +215,31 @@ public class G extends JFrame {
 		BufferStrategy strategy = getBufferStrategy();
 
 		// ok now on screen
-		setIconImage(playerImage);
+		setIconImage(tiles[TILE_GOBLIN_PLAY]);
 		setVisible(true);
 
 		long nextFrameStart = System.nanoTime();
 		while (true) {
-			
+
 			stepCounter++;
 			nextFrameStart += 16666667;
 
 			if (gameState == STATE_NEW_GAME) {
-				
+
 				level = 0;
 				score = 0;
-				
+
 				playerSpeed = 30;
 			}
 
-			if(gameState == STATE_LEVEL_INIT) {
+			if (gameState == STATE_LEVEL_INIT) {
 
 				facesCount = 0;
 				rocksCount = 0;
 
 				level++;
 				playerSpeed -= 2;
-				if(playerSpeed < 0) {
+				if (playerSpeed < 0) {
 					playerSpeed = 0;
 				}
 
@@ -267,7 +251,7 @@ public class G extends JFrame {
 				gameState = STATE_LEVEL_BUILD;
 			}
 
-			if(gameState == STATE_LEVEL_BUILD) {
+			if (gameState == STATE_LEVEL_BUILD) {
 				// LEVEL INITIALIZATION
 
 				if (rocksCount < MAX_ROCKS) {
@@ -283,8 +267,8 @@ public class G extends JFrame {
 				if (facesCount < MAX_FACES) {
 
 					do {
-						x = (int)(Math.random() * BOARD_WIDTH);
-						y = (int)(Math.random() * BOARD_HEIGHT - 1);
+						x = (int) (Math.random() * BOARD_WIDTH);
+						y = (int) (Math.random() * BOARD_HEIGHT - 1);
 					} while (board[x][y] != TILE_NOTHING);
 					board[x][y] = TILE_FACE_SAD;
 					facesCount++;
@@ -294,13 +278,11 @@ public class G extends JFrame {
 				playerY = BOARD_HEIGHT - 1;
 
 				if ((rocksCount >= MAX_ROCKS) && (facesCount >= MAX_FACES)) {
-					
-					gameState = STATE_PLAYING;				
-				}
-				
-			}
 
-			
+					gameState = STATE_PLAYING;
+				}
+
+			}
 
 			/*****************************************************************
 			 * EVOLUTION
@@ -328,6 +310,8 @@ public class G extends JFrame {
 			}
 
 			if (gameState == STATE_PLAYING) {
+
+				board[playerX][playerY] = TILE_NOTHING;
 
 				if (key[KeyEvent.VK_LEFT] && !keyLock[KeyEvent.VK_LEFT]) {
 					playerX -= 1;
@@ -360,40 +344,36 @@ public class G extends JFrame {
 					playerX -= BOARD_WIDTH;
 				}
 
-				
 				if (board[playerX][playerY] == TILE_FACE_SAD) {
 
-					board[playerX][playerY] = TILE_NOTHING;
 					facesCount--;
 
 					score += 100;
 
-					/*
-					channel.noteOn(60, 50);
-					channel.noteOff(60);
-					*/
-		
-				
-					AudioPlayer.player.start(tone1);
-				
-		
+					AudioPlayer.player.start(createSample());
+
 					if (facesCount == 0) {
 						gameState = STATE_LEVEL_INIT;
 					}
 				}
-				
-				if (board[playerX][playerY] == TILE_ROCK) {
 
-					channel.noteOn(22, 80);
-					channel.noteOff(22);
+				if (board[playerX][playerY] == TILE_ROCK) {
 
 					gameState = STATE_GAME_OVER;
 				}
+			}
 
+			if (gameState == STATE_GAME_OVER) {
+
+				board[playerX][playerY] = TILE_GOBLIN_CRASH;
+
+			} else {
+
+				board[playerX][playerY] = TILE_GOBLIN_PLAY;
 			}
 
 			if (key[KeyEvent.VK_ENTER]) {
-				
+
 				if (gameState == STATE_NEW_GAME) {
 					gameState = STATE_LEVEL_INIT;
 				}
@@ -406,7 +386,7 @@ public class G extends JFrame {
 			/******************************************************************
 			 * SCREEN UPDATE
 			 ******************************************************************/
-			g = (Graphics2D)strategy.getDrawGraphics();
+			g = (Graphics2D) strategy.getDrawGraphics();
 			g.translate(getInsets().left, getInsets().top);
 			g.drawImage(scenery, 0, 0, null);
 
@@ -422,26 +402,22 @@ public class G extends JFrame {
 				}
 			}
 
-			// Draw player
-			g.drawImage(playerImage, playerX * TILE_SIZE, playerY * TILE_SIZE,
-					null);
-
 			setTitle(String.format(
 					"Goblin4k -- Level: %02d(%02d faces) Score: %06d / %06d ",
 					level, facesCount, score, bestScore));
 
-			if(gameState == STATE_NEW_GAME) {
+			if (gameState == STATE_NEW_GAME) {
 				g.setColor(new Color(0xBBABCDEF, true));
 				g.fillRect(0, (int) (getHeight() * 0.7), getWidth(), 30);
 				g.setColor(Color.BLACK);
 				String msg = "Goblin4k - Press ENTER to start";
 				g.setFont(new Font("sansserif", Font.BOLD, 14));
 				g.drawString(msg, (GAMEFILED_WIDTH - g.getFontMetrics()
-						.stringWidth(msg)) / 2, (int) (getHeight() * 0.7)
-						+ 14+(30 - 14) / 2);
+						.stringWidth(msg)) / 2, (int) (getHeight() * 0.7) + 14
+						+ (30 - 14) / 2);
 			}
 
-			if(gameState == STATE_GAME_OVER) {
+			if (gameState == STATE_GAME_OVER) {
 
 				g.setColor(new Color(0xBBABCDEF, true));
 				g.fillRect(0, (int) (getHeight() * 0.7), getWidth(), 30);
@@ -449,8 +425,8 @@ public class G extends JFrame {
 				String msg = "GAME OVER - Press ENTER to start";
 				g.setFont(new Font("sansserif", Font.BOLD, 14));
 				g.drawString(msg, (GAMEFILED_WIDTH - g.getFontMetrics()
-						.stringWidth(msg)) / 2, (int) (getHeight() * 0.7)
-						+ 14+(30 - 14) / 2);
+						.stringWidth(msg)) / 2, (int) (getHeight() * 0.7) + 14
+						+ (30 - 14) / 2);
 			}
 
 			strategy.show();
@@ -471,139 +447,59 @@ public class G extends JFrame {
 		if (e.getID() == KeyEvent.KEY_RELEASED) {
 			keyLock[e.getKeyCode()] = false;
 		}
-		
+
 	}
 
-	private static final void drawGoblin(Graphics2D g) {
+	public static void drawSprite(Graphics2D g, Color color, int[] values) {
 
-		g.setColor(Color.green.darker());
+		g.setColor(color);
 
-		g.fillRect(1 * PIXEL_SIZE, 0 * PIXEL_SIZE, 6 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
 
-		g.fillRect(0 * PIXEL_SIZE, 1 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-				3 * PIXEL_SIZE);
-		g.fillRect(3 * PIXEL_SIZE, 1 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-				3 * PIXEL_SIZE);
-		g.fillRect(6 * PIXEL_SIZE, 1 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-				3 * PIXEL_SIZE);
+				if ((values[i] & 1 << j) != 0) {
 
-		g.fillRect(0 * PIXEL_SIZE, 3 * PIXEL_SIZE, 8 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-
-		g.fillRect(0 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(2 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(5 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(7 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-
-		g.fillRect(1 * PIXEL_SIZE, 5 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				2 * PIXEL_SIZE);
-		g.fillRect(3 * PIXEL_SIZE, 5 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-				2 * PIXEL_SIZE);
-		g.fillRect(6 * PIXEL_SIZE, 5 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				2 * PIXEL_SIZE);
-
-		g.fillRect(0 * PIXEL_SIZE, 7 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(2 * PIXEL_SIZE, 7 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(5 * PIXEL_SIZE, 7 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(7 * PIXEL_SIZE, 7 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-	}
-
-	private static final void drawRock(Graphics2D g) {
-
-		g.setColor(Color.ORANGE);
-
-		g.fillRect(1 * PIXEL_SIZE, 1 * PIXEL_SIZE, 6 * PIXEL_SIZE,
-				6 * PIXEL_SIZE);		
-	}
-
-	private static final void drawFace(Graphics2D g, boolean happinessFlag) {
-
-		g.setColor(Color.BLACK);
-
-		g.fillRect(2 * PIXEL_SIZE, 0 * PIXEL_SIZE, 4 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(1 * PIXEL_SIZE, 1 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(6 * PIXEL_SIZE, 1 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(0 * PIXEL_SIZE, 2 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				4 * PIXEL_SIZE);
-		g.fillRect(7 * PIXEL_SIZE, 2 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				4 * PIXEL_SIZE);
-		g.fillRect(2 * PIXEL_SIZE, 2 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(5 * PIXEL_SIZE, 2 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-
-		g.fillRect(1 * PIXEL_SIZE, 6 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(6 * PIXEL_SIZE, 6 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-		g.fillRect(2 * PIXEL_SIZE, 7 * PIXEL_SIZE, 4 * PIXEL_SIZE,
-				1 * PIXEL_SIZE);
-
-		if (happinessFlag) {
-			g.fillRect(2 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
-			g.fillRect(5 * PIXEL_SIZE, 4 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
-			g.fillRect(3 * PIXEL_SIZE, 5 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
-		} else {
-			g.fillRect(2 * PIXEL_SIZE, 5 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
-			g.fillRect(5 * PIXEL_SIZE, 5 * PIXEL_SIZE, 1 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
-			g.fillRect(3 * PIXEL_SIZE, 4 * PIXEL_SIZE, 2 * PIXEL_SIZE,
-					1 * PIXEL_SIZE);
+					g.fillRect(j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE,
+							PIXEL_SIZE);
+				}
+			}
 		}
-
 	}
 
-	
 	private static final InputStream createSample() {
-		
-		
+
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(baos);
-		
+
 		try {
-			dos.writeInt(0x2e736e64);		//magic number ".snd"
-			dos.writeInt(24);				//data offset
-			dos.writeInt(0xffffffff);		//data size (-1 unknown)
-			dos.writeInt(3);				//data format
-			dos.writeInt(8000);				//sample rate
-			dos.writeInt(1);				//channels
-						
-			double[] samples = tone(800, 0.1, 1.0, 8000);
-			for(int i=0; i<samples.length; i++) {
-				dos.writeShort((short)(Short.MAX_VALUE*samples[i]));
+			dos.writeInt(0x2e736e64); // magic number ".snd"
+			dos.writeInt(24); // data offset
+			dos.writeInt(0xffffffff); // data size (-1 unknown)
+			dos.writeInt(3); // data format
+			dos.writeInt(16000); // sample rate
+			dos.writeInt(1); // channels
+
+			double[] samples = tone(800, 0.08, 0.5, 16000);
+			for (int i = 0; i < samples.length; i++) {
+				dos.writeShort((short) (Short.MAX_VALUE * samples[i]));
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return new ByteArrayInputStream(baos.toByteArray());
 	}
-	
-	private static double[] tone(double hz, double duration, double amplitude, int sampleRate) {
-		
-        int N = (int)(sampleRate * duration);
-        double[] a = new double[N+1];
-        for (int i = 0; i <= N; i++)
-            a[i] = amplitude * Math.sin(2 * Math.PI * i * hz / sampleRate);
-        return a;
-    }
-	
+
+	private static double[] tone(double hz, double duration, double amplitude,
+			int sampleRate) {
+
+		int N = (int) (sampleRate * duration);
+		double[] a = new double[N + 1];
+		for (int i = 0; i <= N; i++)
+			a[i] = amplitude * Math.sin(2 * Math.PI * i * hz / sampleRate);
+		return a;
+	}
+
 }
